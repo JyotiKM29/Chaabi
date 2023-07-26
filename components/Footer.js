13a980aaa39c5e916ad1cee4d4e21472bc5b9f38
JyotiKM29 <jyotikumari4442@gmail.com>
2023-07-26 13:02:58 +0530

    Footer is added

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,6 @@
-import styles from '../styles/component/footer.module.scss';
+import Image from "next/image";
+import styles from "../styles/component/footer.module.scss";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -12,57 +14,84 @@ const Footer = () => {
         </p>
       </div>
       <div className={styles.footer__bottom}>
-        <div className={styles.footer__bottom_logo}>
-          {/* Logo */}
-          <h1>Chaabi</h1>
-          <button>Download Now</button>
-        </div>
-        <div className={styles.footer__bottom_list}>
-          <h3 className={styles.footer__bottom_heading}>Product</h3>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
+        <div className={styles.footer__bottom__upper}>
+          <div className={styles.footer__bottom__upper_logo}>
+            {/* Logo */}
+            <h1>Chaabi</h1>
+            <span>for Bluecollar Works</span>
+          </div>
+          <div className={styles.footer__bottom__upper_list}>
+            <h3>Product</h3>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+          </div>
+
+          {/* product */}
+          <div className={styles.footer__bottom__upper_list}>
+            <h3>Product</h3>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+          </div>
+
+          {/* Support */}
+          <div className={styles.footer__bottom__upper_list}>
+            <h3>Product</h3>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+          </div>
+
+          {/* Get in Touch */}
+          <div className={styles.footer__bottom__upper_list}>
+            <h3>Product</h3>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+            <h5>Features</h5>
+          </div>
         </div>
 
-        {/* product */}
-        <div className={styles.footer__bottom_list}>
-          <h3 className={styles.footer__bottom_heading}>Product</h3>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
+        <div className={styles.footer__bottom__lower}>
+          <div className={styles.button}>
+            <button>Download Now</button>
+          </div>
+
+          {/* Social Handle */}
+          <div className={styles.social}>
+            <Link href="#">
+              <Image src={"/instagram.svg"} width={50} height={50} />
+            </Link>
+            <Link href="#">
+              <Image src={"/facebook.svg"} width={50} height={50} />
+            </Link>
+            <Link href="#">
+              <Image src={"/linkedin.svg"} width={50} height={50} />
+            </Link>
+            <Link href="#">
+              <Image src={"/youtube.svg"} width={50} height={50} />
+            </Link>
+          </div>
         </div>
 
-        {/* Support */}
-        <div className={styles.footer__bottom_list}>
-          <h3 className={styles.footer__bottom_heading}>Product</h3>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-        </div>
-
-        {/* Get in Touch */}
-        <div className={styles.footer__bottom_list}>
-          <h3 className={styles.footer__bottom_heading}>Product</h3>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-        </div>
-
-        <div className={styles.footer__bottom_list}>
-          <h3 className={styles.footer__bottom_heading}>Product</h3>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
-          <h5 className={styles.footer__bottom_subheading}>Features</h5>
+        <hr className={styles.hr}/>
+        <div className={styles.rights} >
+          <div className={styles.rights__left}>
+            <span>Cookies</span>
+            <span>Cookies Polices</span>
+            <span>Private Policy</span>
+            <span>Terms</span>
+            
+          </div>
+          <div className={styles.rights__right}>2023 © chaabi. All rights reserved. Powered by WorkRyt Technologies</div>
         </div>
       </div>
     </div>
